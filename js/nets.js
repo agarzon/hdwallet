e0ae7679d4bed6194b417e1ba5a0a8ac8cf64694
Alexander Garzon <venehosting@gmail.com>
2017-10-25 18:00:35 -0400

    WIP

---
+++ js/nets.js
@@ -0,0 +1,12 @@
+var PotNet = {
+    name: 'potcoin',
+    alias: 'potcoin',
+    pubkeyhash: 0x37,
+    privatekey: 0xB7,
+    scripthash: 0x05,
+    xpubkey: 0x213045C1,
+    xprivkey: 0x213045C0,
+    port: 4200
+};
+
+// Yeap.... you can add more coins here
