25c8e1fa5ee7c896e4e3d41fa9257f33176cbcc0
Alexander Garzon <venehosting@gmail.com>
2017-11-10 10:17:00 -0500

    some normalization

--- js/wallet.js
+++ js/wallet.js
@@ -1,111 +1,111 @@
 var Wallet = (function() {
-  'use strict';
+    'use strict';
 
-  const bitcore = require('bitcore-lib');
-  const explorers = require('bitcore-explorers');
-  const mnemonic = require('bitcore-mnemonic');
+    var bitcore = require('bitcore-lib');
+    var explorers = require('bitcore-explorers');
+    var mnemonic = require('bitcore-mnemonic');
 
-  const maxAddresses = 100;
-  var addresses = [];
+    var maxAddresses = 10;
+    var addresses = [];
 
-  /**
-   * Here lies all the secret... change this lines to make it work with any other altcoin.
-   */
-  var derivationPath = "m/44'/81'/0'/0";
-  var blockchainUrl = 'https://chain.potcoin.com';
-  var PotNet = {
-    name: 'potcoin',
-    alias: 'potcoin',
-    pubkeyhash: 0x37, // 55
-    privatekey: 0xB7, // 183 (wif)
-    scripthash: 0x05, // 5
-    xpubkey: 0x0488b21e,
-    xprivkey: 0x0488ade4,
-    port: 4200
-  };
+    /**
+     * Here lies all the secret... change this lines to make it work with any other altcoin.
+     */
+    var derivationPath = "m/44'/81'/0'/0";
+    var blockchainUrl = 'https://chain.potcoin.com';
+    var PotNet = {
+        name: 'potcoin',
+        alias: 'potcoin',
+        pubkeyhash: 0x37, // 55
+        privatekey: 0xB7, // 183 (wif)
+        scripthash: 0x05, // 5
+        xpubkey: 0x0488b21e,
+        xprivkey: 0x0488ade4,
+        port: 4200
+    };
 
-  function generateNemo() {
-    var code = new mnemonic();// English words
-    var nemo = code.toString();
-    console.log(nemo);
-    return nemo;
-  }
-
-  function generateHD(nemo, pass) {
-    if (validatesNemo(nemo) === false) {
-      throw new Error("Seed code is invalid");
+    function generateNemo() {
+        var code = new mnemonic(); // English words
+        var nemo = code.toString();
+        console.log(nemo);
+        return nemo;
     }
 
-    var code = new mnemonic(nemo);
-    console.log(code);
-    var xpriv = code.toHDPrivateKey(pass);
-    console.log(xpriv);
-    var hdPrivateKey = new bitcore.HDPrivateKey(xpriv);
-    console.log(hdPrivateKey);
-    //var hdPublicKey = hdPrivateKey.hdPublicKey;
-    //console.log(hdPublicKey); // kind of useless, since won't be used for derivation
+    function generateHD(nemo, pass) {
+        if (validatesNemo(nemo) === false) {
+            throw new Error("Seed code is invalid");
+        }
 
-    for (var i = 0; i < maxAddresses; i++) {
-      var derived = hdPrivateKey.derive(derivationPath).derive(i);
-      var address = new bitcore.Address(derived.publicKey).toString();
-      var privatekey = derived.privateKey.toWIF();
-      var balance = 0;
-      addresses.push({address, privatekey, balance});
+        var code = new mnemonic(nemo);
+        console.log(code);
+        var xpriv = code.toHDPrivateKey(pass);
+        console.log(xpriv);
+        var hdPrivateKey = new bitcore.HDPrivateKey(xpriv);
+        console.log(hdPrivateKey);
+        //var hdPublicKey = hdPrivateKey.hdPublicKey;
+        //console.log(hdPublicKey); // kind of useless, since won't be used for derivation
+
+        for (var i = 0; i < maxAddresses; i++) {
+            var derived = hdPrivateKey.derive(derivationPath).derive(i);
+            var address = new bitcore.Address(derived.publicKey).toString();
+            var privatekey = derived.privateKey.toWIF();
+            var balance = 0;
+            addresses.push({ address: address, privatekey: privatekey, balance: balance });
+        }
+
+        return addresses;
     }
 
-    return addresses
-  }
+    /**
+     * Validations
+     */
+    function validatesNemo(nemo) {
+        if (typeof nemo !== 'undefined') {
+            return mnemonic.isValid(nemo);
+        }
 
-  /**
-  * Validations
-  */
-  function validatesNemo(nemo) {
-    if (typeof nemo !== 'undefined') {
-      return mnemonic.isValid(nemo);
+        throw new Error("Where is the seed to validate?");
     }
 
-    throw new Error("Where is the seed to validate?");
-  }
+    function validatesPriv(privKey) {
+        if (typeof privKey !== 'undefined') {
+            return bitcore.PrivateKey.isValid(privKey);
+        }
 
-  function validatesPriv(privKey) {
-    if (typeof privKey !== 'undefined') {
-      return bitcore.PrivateKey.isValid(privKey);
+        throw new Error("No private key provided");
     }
 
-    throw new Error("No private key provided");
-  }
+    function validatesPub(pubKey) {
+        if (typeof pubKey !== 'undefined') {
+            return bitcore.PublicKey.isValid(pubKey);
+        }
 
-  function validatesPub(pubKey) {
-    if (typeof pubKey !== 'undefined') {
-      return bitcore.PublicKey.isValid(pubKey);
+        throw new Error("No public key provided");
     }
 
-    throw new Error("No public key provided");
-  }
+    function validatesAddress(address) {
+        if (typeof address !== 'undefined') {
+            return bitcore.Address.isValid(address);
+        }
 
-  function validatesAddress(address) {
-    if (typeof address !== 'undefined') {
-      return bitcore.Address.isValid(address);
+        throw new Error("No address provided");
     }
 
-    throw new Error("No address provided");
-  }
+    // main init method
+    function init() {
+        var AltNet = new bitcore.Networks.add(PotNet); // Set potcoin network as default
+        bitcore.Networks.defaultNetwork = AltNet;
+    }
 
-  // main init method
-  function init() {
-    var AltNet = new bitcore.Networks.add(PotNet);// Set potcoin network as default
-    bitcore.Networks.defaultNetwork = AltNet;
-  }
-
-  /* =============== export public methods =============== */
-  return {
-    init: init,
-    generateNemo: generateNemo,
-    validatesNemo: validatesNemo, // Useful only with generateHD
-    validatesPriv: validatesPriv, // Useful only to import
-    validatesPub: validatesPub, // Kind of useless
-    validatesAddress: validatesAddress,
-    generateHD: generateHD,
-    addresses: addresses,
-  };
+    /* =============== export public methods =============== */
+    return {
+        init: init,
+        generateNemo: generateNemo,
+        validatesNemo: validatesNemo, // Useful only with generateHD
+        validatesPriv: validatesPriv, // Useful only to import
+        validatesPub: validatesPub, // Kind of useless
+        validatesAddress: validatesAddress,
+        generateHD: generateHD,
+        addresses: addresses,
+    };
 }());
--- rpc.php
+++ rpc.php
@@ -1,18 +1,40 @@
 <pre>
 <?php
 
+ini_set('xdebug.var_display_max_depth', 15);
+ini_set('xdebug.var_display_max_children', 256);
+ini_set('xdebug.var_display_max_data', 1024);
+
 include 'php/jsonRPCClient.php';
-include 'php/EasyBitcoin-PHP.php';
+//include 'php/EasyBitcoin-PHP.php';
 
 //$bitcoin = new Bitcoin('potcoinrpc', '2Mdo2p2m4DkdeSNvvtSKjxBinnmKRYib1hdWdWJcBqZQ', '138.197.151.169', '42000');
 //var_dump($bitcoin->sendrawtransaction('01000000017f85b7a328f0fc0a3a66e0864639189ac0a4873ea98be3d463c806217a7e0c5f0100000049483045022100da526a9275beddedeb6ce4ef9f105d8d7a68eda5df5e58eccbb8bc8f0169f22602205cd061e95199d9fbb854cc6f1ae3408e274747eda69a9be76050a0c47e8ff5f301ffffffff02401f0000000000001976a91461c652988f9ffe202e2fe640014a6296a491ff2488ac6b3fc01d000000001976a91486e276e2aaf23425fa413a8f67b16c75e17dcfd588ac00000000'));
 //var_dump($bitcoin->getinfo());
 
 $bitcoin = new jsonRPCClient('http://potcoinrpc:2Mdo2p2m4DkdeSNvvtSKjxBinnmKRYib1hdWdWJcBqZQ@138.197.151.169:42000/', true);
+//var_dump($bitcoin->getinfo());
 
-$txraw = '01000000019a5a44c28acdfe1b360e565a3e3b396bc155525e40b333175775fca04fa073d9010000006a473044022045eda587f38461e391b93de220e26afb3e927d6473e6a3ae48e150ca3f992f120220292b1558facd07936ddd855c5377a9c193ef2cedf97fa67c0d32a9a5a15eb67f0121031e4d9fb58a451822fdf9b4fea0e22c33a685097a65a8e8760730dcb6929680e7ffffffff0250c30000000000001976a91452cfece0004d525df6c3d85295601a5b196ebaa988ac500a8700000000001976a9144d515dfc23e9a97de075309be0011ef7e44230d388ac00000000';
-//print_r($bitcoin->decoderawtransaction($txraw));
-print_r($bitcoin->sendrawtransaction($txraw));
-var_dump($bitcoin->getinfo());
+$utox = '[{"address":"PFdzfENGEYdfXn6S8AZvRSijKpHixautkq","txid":"6adad616d0c36488ec3dbbe3e601c60d4ac5d5e29123e43da1ce0d83f3b4839a","vout":0,"ts":1510174436,"scriptPubKey":"76a9144d515dfc23e9a97de075309be0011ef7e44230d388ac","amount":0.087,"confirmations":6,"confirmationsFromCache":true}]';
+$json = json_decode($utox, true);
+
+$destinations = [
+    "PG93ec6KmpYKcssFPMW7jKV9uPcAw35SEE" => 0.01,
+    "PFdzfENGEYdfXn6S8AZvRSijKpHixautkq" => 0.076
+];
+
+$txraw1 = $bitcoin->createrawtransaction($json, $destinations);
+var_dump($txraw1);
+$hex1 = $bitcoin->decoderawtransaction($txraw1);
+var_dump($hex1);
+
+
+$signed = $bitcoin->signrawtransaction($txraw1, $json, ["U7Wig5kPqXNN34dLN568hVk9sLsWxK1AbCdex1oRJwCsbFVxUM6K"]);
+var_dump($signed);
+$signedDeco = $bitcoin->decoderawtransaction($signed);
+var_dump($signedDeco);
+
+//var_dump($bitcoin->sendrawtransaction($txraw));
+//var_dump($bitcoin->getreceivedbylabel());
 ?>
 </pre>
